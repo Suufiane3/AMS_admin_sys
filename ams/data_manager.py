3e2ba0f2dcd99bb14d49855931e81b2ffeaf7a1a
Suufiane3 <96297869+Suufiane3@users.noreply.github.com>
2025-03-28 13:43:44 +0100

    Update data_manager.py

--- ams/data_manager.py
+++ ams/data_manager.py
@@ -26,21 +26,22 @@ def insert_json_to_db(json_file):
     cursor = conn.cursor()
 
     with open(json_file, 'r', encoding='utf-8') as file:
-    for line in file:
-        line = line.strip()  # Enlever les espaces et sauts de ligne
-        if not line:
-            continue  # Ignorer les lignes vides
-        
-        try:
-            data = json.loads(line)  # Convertir la ligne en JSON
-        except json.JSONDecodeError:
-            print(f" Erreur JSON sur la ligne : {line}")
-            continue  # Ignorer la ligne si elle est invalide
-    
-        cursor.execute("INSERT INTO data_archive (data) VALUES (?)", (json.dumps(data),))
-    
+        for line in file:
+            line = line.strip()  # Enlever les espaces et sauts de ligne
+            if not line:
+                continue  # Ignorer les lignes vides
+
+            try:
+                data = json.loads(line)  # Convertir la ligne en JSON
+            except json.JSONDecodeError:
+                print(f" Erreur JSON sur la ligne : {line}")
+                continue  # Ignorer la ligne si elle est invalide
+
+            cursor.execute("INSERT INTO data_archive (data) VALUES (?)", (json.dumps(data),))
+
     conn.commit()
     conn.close()
 
 
 insert_json_to_db("data.json")
+
