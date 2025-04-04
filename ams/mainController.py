0265afdc8ca2fc19fb6eee0d82389b1f59465167
Suufiane3 <96297869+Suufiane3@users.noreply.github.com>
2025-04-04 14:56:09 +0200

    Update mainController.py

--- ams/mainController.py
+++ ams/mainController.py
@@ -3,16 +3,18 @@ import ram_info
 import data_manager
 import scraper
 import subprocess
+import crisis_detector
 
 f = "data.json"
-
 
 cpu_info.dump_cpu_info()
 ram_info.dump_ram_info()
 subprocess.run(["bash", "storage_info.sh"])
+
+crisis_detector.dump_crisis_status()
 scraper.dump_alerte()
 
 data_manager.create_table()
 data_manager.insert_json_to_db(f)
 data_manager.clear_json_file(f)
-print("Tâche terminé")
+print("Tâche terminée")
