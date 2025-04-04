bac5bc03f76a19faf7dfb876f2a6218f58dcea06
Suufiane3 <96297869+Suufiane3@users.noreply.github.com>
2025-04-04 13:41:33 +0200

    Update mainController.py

--- ams/mainController.py
+++ ams/mainController.py
@@ -1,15 +1,18 @@
-import cpu_info.py
-import ram_info.py
-import data_manager.py
+import cpu_info
+import ram_info
+import data_manager
+import scraper
 import subprocess
+
+f = "data.json"
 
 
 cpu_info.dump_cpu_info()
 ram_info.dump_ram_info()
+subprocess.run(["bash", "storage_info.sh"])
+scraper.dump_alerte()
 
 data_manager.create_table()
-print("Aucune table existante, creation avec succès")
-data_manager.insert_json_to_db()
+data_manager.insert_json_to_db(f)
+data_manager.clear_json_file(f)
 print("Tâche terminé")
-
-
